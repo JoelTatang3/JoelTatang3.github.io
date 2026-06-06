2daad17db64280927593a0ac3b39c925fa9c0d4c
JoelTatang3 <147861076+JoelTatang3@users.noreply.github.com>
2026-06-06 17:20:29 +0000

    Deploying to gh-pages from @ JoelTatang3/JoelTatang3.github.io@7acb829e590e987441f1f16462952eaa38a7d418 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -494,25 +494,11 @@ ninja.data = [{
           window.open("https://github.com/JoelTatang3", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
           window.open("https://www.linkedin.com/in/jo%C3%ABl-tatang", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
         },
       },{
         id: 'social-scholar',
